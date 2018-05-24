ca024803d2f37ac6f4df52ee1559088988a708e5
Rauf <m_rauf@outlook.com>
2018-05-24 09:57:02 -0400

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -2,10 +2,12 @@ public class InsertionSort {
     
     public static int[] insertionSort(int[] inputArray) {
         
+        int index;
+        
         for(int i = 0; i < inputArray.length; i++) {
             
             //Select the number
-            int index = i;
+            index = i;
             
             //Check numbers below the selected number
             for(int j = i; j > 0; j--) {
