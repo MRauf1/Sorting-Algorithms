bea5390a6a129a27d5d14b2cd8dc7fdb69feb427
Rauf <m_rauf@outlook.com>
2018-05-24 09:42:38 -0400

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -3,11 +3,18 @@ public class BubbleSort {
     public static int[] bubbleSort(int[] inputArray) {
         
         for(int i = 0; i < inputArray.length; i++) {
+            
             for(int j = 0; j < (inputArray.length - i) - 1; j++) {
+            
+                //Swap if the number is out of place
                 if(inputArray[j] > inputArray[j + 1]) {
+            
                     HelperMethods.swap(inputArray, j, j + 1);
+            
                 }
+            
             }
+        
         }
         
         return inputArray;
