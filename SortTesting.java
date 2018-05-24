d49f597efca90215706987f67bed315d7201729e
Rauf <m_rauf@outlook.com>
2018-05-24 09:46:19 -0400

    Update SortTesting.java

--- SortTesting.java
+++ SortTesting.java
@@ -4,16 +4,24 @@ public class SortTesting {
     
     public static void main(String[] args) {
         
+        //Initialize the testing array and fill it with random integers
         int[] testArray = new int[100];
         
         for(int i = 0; i < testArray.length; i++) {
+            
             testArray[i] = (int)(Math.random() * 1000);
+        
         }
         
-        int[] outputArray = BubbleSort.bubbleSort(testArray);
+        //Sort the array
+        //int[] outputArray = BubbleSort.bubbleSort(testArray);
+        int[] outputArray = InsertionSort.insertionSort(testArray);
         
+        //Print out the result
         for(int i = 0; i < outputArray.length; i++) {
+        
             System.out.println(outputArray[i]);
+        
         }
         
     }
