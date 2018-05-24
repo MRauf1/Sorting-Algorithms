0a4624953d3d6f9f8542982a38ecbcc8a1952f84
Rauf <m_rauf@outlook.com>
2018-05-24 09:41:16 -0400

    Create InsertionSort.java

---
+++ InsertionSort.java
@@ -0,0 +1,29 @@
+public class InsertionSort {
+    
+    public static int[] insertionSort(int[] inputArray) {
+        
+        for(int i = 0; i < inputArray.length; i++) {
+            
+            //Select the number
+            int index = i;
+            
+            //Check numbers below the selected number
+            for(int j = i; j > 0; j--) {
+            
+                if(inputArray[index] < inputArray[j - 1]) {
+                    
+                    //Swap and keep track of the selected number
+                    HelperMethods.swap(inputArray, index, j - 1);
+                    index = j - 1;
+            
+                }
+            
+            }
+
+        }
+        
+        return inputArray;
+        
+    }
+    
+}
