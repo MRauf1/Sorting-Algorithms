d9720737cb3ce308766eea680b593213e93e0dd0
Rauf <m_rauf@outlook.com>
2018-05-25 10:19:16 -0400

    Update SortTesting.java

--- SortTesting.java
+++ SortTesting.java
@@ -2,14 +2,17 @@ import java.lang.Math;
 
 public class SortTesting {
     
+    private final static int SIZE = 100;
+    private final static int MAX_VALUE = 1000;
+    
     public static void main(String[] args) {
         
         //Initialize the testing array and fill it with random integers
-        int[] testArray = new int[100];
+        int[] testArray = new int[SIZE];
         
         for(int i = 0; i < testArray.length; i++) {
             
-            testArray[i] = (int)(Math.random() * 1000);
+            testArray[i] = (int)(Math.random() * MAX_VALUE);
         
         }
         
@@ -17,7 +20,8 @@ public class SortTesting {
         //int[] outputArray = BubbleSort.bubbleSort(testArray);
         //int[] outputArray = InsertionSort.insertionSort(testArray);
         //int[] outputArray = SelectionSort.selectionSort(testArray);
-        int[] outputArray = MergeSort.mergeSort(testArray, 0, testArray.length);
+        //int[] outputArray = MergeSort.mergeSort(testArray, 0, testArray.length);
+        int[] outputArray = CountingSort.countingSort(testArray, MAX_VALUE);
         
         //Print out the result
         for(int i = 0; i < outputArray.length; i++) {
